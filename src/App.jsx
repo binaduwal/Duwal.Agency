20dbbabf0646a66e88c36f8a014011583579a52d
Bina Duwal <duwalbina2@gmail.com>
2025-06-29 21:24:47 +0545

    Background gradient effect

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-    </>
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-20 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[88px]  -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
   );
 }
 
